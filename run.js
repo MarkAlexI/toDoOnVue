a36ab24d58896bfb6a760ff575363dd5b066cb1b
MarkAlexI <markovaleksandr108@gmail.com>
2022-06-18 16:41:27 +0300

    Styling application.

--- run.js
+++ run.js
@@ -19,8 +19,8 @@ const app = Vue.createApp({
 app.component('todoitem', {
   props: ['todo', 'index'],
   template: `<div>
-              <p>Number of record: {{index + 1}}
-              <br> Title: {{todo.title}} <br> Text: {{todo.comment}} </p>
+              <p><span>Number of record: </span>{{index + 1}}
+              <br> <span>Title: </span>{{todo.title}} <br> <span>Text: </span>{{todo.comment}} </p>
               <button v-on:click="todoDelete(index)">Delete</button>
             </div>`,
   methods: {
