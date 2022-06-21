b26f07c5b8c49c7725bc12a95ddf3305fd61e029
MarkAlexI <markovaleksandr108@gmail.com>
2022-06-21 08:48:03 +0300

    Refact 'setId'.

--- run.js
+++ run.js
@@ -68,7 +68,12 @@ function setId() {
   const dict = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const rand = () => Math.floor(Math random() * dict.length);
+
+  const rand = () => {
+    const index = Math.floor(Math.random() * dict.length);
+    return dict[index];
+  };
+
   const repeat = (f, times) => {
     let result = '';
     for (let i = 0; i < times; i++) {
@@ -76,6 +81,6 @@ function setId() {
     }
     return result;
   }
+
   return repeat(rand, 9);
 }
-console.log(setId());
